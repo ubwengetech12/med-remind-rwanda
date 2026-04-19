b5023675314d6d7df2943ec6ac1f57791e1bd4ab
ubwenge-tech <techubwenge@gmail.com>
2026-04-19 14:40:13 +0200

    Delete patient-app directory

--- patient-app/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.medwise.app',
-  appName: 'MedWise',
-  webDir: 'out'
-};
-
-export default config;
